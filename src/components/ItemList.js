712381b574e5eb96e44a13b1bc09cc599adee073
kulanah <devonthebutler@gmail.com>
2018-07-10 14:48:26 -0700

    created default placement and text of items list

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import './css/ItemList.css';
 
 class ItemList extends React.Component{
   constructor(){
     super();
+    this.state = ({
+      items: [],
+    });
+
+    this.displayItems = this.displayItems.bind(this);
+  }
+
+  displayItems(){
+    if (this.state.items.length === 0){
+      return(
+        <p>There are currently no items in the scene</p>
+      );
+    } else {
+      return (
+        /*map of items in list */
+        <p>this is where the lsit goes</p>
+      )
+    }
   }
 
   render(){
     return (
-      <div>TEST</div>
+      <div id='itemListDiv'>{this.displayItems()}</div>
     )
   }
 }
