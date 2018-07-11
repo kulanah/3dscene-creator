6a32a84cda0669579fc873295479c47d846a2157
kulanah <devonthebutler@gmail.com>
2018-07-11 15:02:53 -0700

    made properties box visible with basic styling

--- src/components/PropertiesBox.js
+++ src/components/PropertiesBox.js
@@ -4,14 +4,25 @@ import './css/PropertiesBox.css';
 class PropertiesBox extends React.Component{
   constructor(){
     super();
+    this.state = ({
+      selectedObject: -1,
+    });
+
+    this.displayItem = this.displayItem.bind(this);
+  }
+
+  displayItem(){
+    if (this.state.selectedObject < 0){
+      return (<h5>Currently no selected object</h5>)
+    } else {
+      return (<p>test</p>)
+    }
   }
 
   render(){
     return (
       <div id='propertiesBoxDiv'>
-        <p>Height: 53</p>
-        <p>Width: 53</p>
-
+        {this.displayItem()}
 
       </div>
     )
