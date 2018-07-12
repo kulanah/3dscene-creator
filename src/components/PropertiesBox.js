cedf58e16e4f765bcc9cd325e542bcfc2729e5ba
kulanah <devonthebutler@gmail.com>
2018-07-12 11:11:21 -0700

    created properites box display function

--- src/components/PropertiesBox.js
+++ src/components/PropertiesBox.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './css/PropertiesBox.css';
 
+import store from '../store';
+
 class PropertiesBox extends React.Component{
   constructor(){
     super();
@@ -8,21 +10,66 @@ class PropertiesBox extends React.Component{
       selectedObject: -1,
     });
 
-    this.displayItem = this.displayItem.bind(this);
+    this.selectItem = this.selectItem.bind(this);
+    this.displaySphere = this.displaySphere.bind(this)
+    this.displayCylinder = this.displayCylinder.bind(this)
   }
 
-  displayItem(){
-    if (this.state.selectedObject < 0){
+  displaySphere(item){
+    return (
+      <div>
+        <p>Sphere</p>
+        <p>Radius: {item.radius}</p>
+      </div>
+    )
+  }
+
+  displayCylinder(item){
+    return (
+      <div>
+        <p>Cylinder</p> 
+        <p>Radius: {item.radius}</p>
+        <p>Height: {item.height}</p>
+      </div>
+    )
+  }
+
+  displayBox(item){
+    return (
+      <div>
+        <p>Box:</p> 
+        <p>Length: {item.radius}</p>
+        <p>Width: {item.width}</p>
+        <p>Height: {item.height}</p>
+      </div>
+    )
+  }
+
+  displayItem(item){
+    switch (item.type){
+      case 'sphere':
+        return this.displaySphere(item);
+      case 'cylinder':
+        return this.displayCylinder(item);
+      default:
+        return (<p>{this.props.selectedItem}</p>)
+    }
+  }
+
+  selectItem(){
+    if (this.props.selectedItem < 0){
       return (<h5>Currently no selected object</h5>)
     } else {
-      return (<p>test</p>)
+      let item = store.getState().createItems[this.props.selectedItem];
+      console.log(item);
+      this.displayItem(item);
     }
   }
 
   render(){
     return (
       <div id='propertiesBoxDiv'>
-        {this.displayItem()}
+        {this.selectItem()}
 
       </div>
     )
