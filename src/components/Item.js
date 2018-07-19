af2adbdd41769b67e47c436a006a10cd9ebadf69
kulanah <devonthebutler@gmail.com>
2018-07-19 16:36:52 -0700

    added stlying for the delete btton and added the onclick for it

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,26 +1,42 @@
 import React from 'react';
 
+import './css/Item.css';
+import trashIcon from '../img/trash.svg';
+
 class Item extends React.Component{
-  constructor(item){
+  constructor(){
     super();
 
     this.clickListItem = this.clickListItem.bind(this);
+    this.clickDelete = this.clickDelete.bind(this);
   }
 
   clickListItem(){
     this.props.listItem(this.props.data.id);
   }
 
+  clickDelete(){
+    this.props.deleteItem(this.props.data.id);
+
+  }
+
   displayType(name){
     let upper = name.charAt(0).toUpperCase() + name.substr(1);
-    return  upper 
+    return upper;
   }
 
   render(){
     let data = this.props.data;
     return (
-      <li className='sceneItems' onClick={this.clickListItem}>{data.id + 1} : {this.displayType(data.type)}</li>
-    )
+      <li className='sceneItems'>
+        <span onClick={this.clickListItem}>
+          {data.id + 1} : {this.displayType(data.type)}
+        </span>
+        <span>
+          <img className='trashIcon' onClick={this.clickDelete} src={trashIcon} alt=''/>
+        </span>
+      </li>
+    );
   }
 } 
 
