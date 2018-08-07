c0a07a3661319e166f921a4119fcb3208ca1bd2b
kulanah <devonthebutler@gmail.com>
2018-08-07 10:10:50 -0700

    Updates

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { createItems } from './createItems';
-
-const rootReducer = combineReducers({
-  createItems, 
-});
-
-export default rootReducer;
