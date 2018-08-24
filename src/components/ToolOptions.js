f07a72a26435634c7bfe1ec11b8772f8fb097aba
kulanah <devonthebutler@gmail.com>
2018-08-24 10:40:23 -0700

    created display for the addition tool

--- src/components/ToolOptions.js
+++ src/components/ToolOptions.js
@@ -3,12 +3,13 @@ import React from 'react';
 import './css/ToolOptions.css';
 import { CreateShape } from './CreateShape';
 import { SegmentShape } from './SegmentShapeOptions';
+import VisibleShapeAddition from './VisibleShapeAddition';
 
 class ToolOptions extends React.Component{
   constructor(){
     super();
 
-    this.toolArray = ['No tool currently selected', <CreateShape/>, <SegmentShape/>];
+    this.toolArray = ['No tool currently selected', <CreateShape/>, <SegmentShape/>, <VisibleShapeAddition/>];
   }
 
   render(){
