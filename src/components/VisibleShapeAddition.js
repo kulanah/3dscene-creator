f3d4ea1a9cd106d9bf53f96c3f1c064109fe6305
kulanah <devonthebutler@gmail.com>
2018-09-10 10:38:22 -0700

    addded the select after combine function to the visible version of shape addition

--- src/components/VisibleShapeAddition.js
+++ src/components/VisibleShapeAddition.js
@@ -1,17 +1,19 @@
 import { selectTool } from 'action/actionCreators';
 import { combineShapes } from 'action/actionCreators';
+import { selectAfterCombine } from 'action/actionCreators';
 
 import { ShapeAddition } from './ShapeAddition';
 
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => ({
-  items: state.items,
+  items: state.itemList,
 });
 
 const mapDispatchToProps = dispatch => ({
   selectTool: (toolId) => dispatch(selectTool(toolId)),
   combineShapes: (shape1, shape2) => dispatch(combineShapes(shape1, shape2)),
+  updateSelectedState: (shape1, shape2) => dispatch(selectAfterCombine(shape1, shape2)),
 });
 
 export default connect(
